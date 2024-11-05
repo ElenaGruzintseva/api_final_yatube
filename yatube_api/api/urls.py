f23e0e71064b1eb141697490d53e17d060e90f13
Elena Gruzintseva <elenagruzinceva@yandex.ru>
2024-11-05 15:43:14 +0300

    version_1

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -14,5 +14,6 @@ router.register('follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path('v1/', include('djoser.urls')),
     path('v1/', include('djoser.urls.jwt')),
 ]
